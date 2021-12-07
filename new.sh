73c03385284370daf7eb492f5656b6dcc4073581
dgarciacosio <dgarciacosio@gmail.com>
2021-12-07 17:14:41 +0100

    Fixed more error and added the fonts for the agnoster theme

--- new.sh
+++ new.sh
@@ -3,6 +3,10 @@ sudo apt install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
 cp zsh_conf/.zshrc ~/
+git clone https://github.com/powerline/fonts
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
 
 # Vi configuration
 git clone https://github.com/Garco97/vi.git
